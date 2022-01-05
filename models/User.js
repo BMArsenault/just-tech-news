fe12e8621aaa7a5b7061267f11d50b2eb525b25f
Brian <BMArsenault@gmail.com>
2022-01-05 12:02:19 -0700

    Added bcrypt and password hook

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {}
@@ -39,18 +40,25 @@ User.init(
       }
     },
     {
+      hooks: {
+         // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+  },
+      },
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
     // pass in our imported sequelize connection (the direct connection to our database)
-    sequelize,
+      sequelize,
     // don't automatically create createdAt/updatedAt timestamp fields
-    timestamps: false,
+      timestamps: false,
     // don't pluralize name of database table
-    freezeTableName: true,
+      freezeTableName: true,
     // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
-    underscored: true,
+      underscored: true,
     // make it so our model name stays lowercase in the database
-    modelName: 'user'
+      modelName: 'user'
     }
 );
 
